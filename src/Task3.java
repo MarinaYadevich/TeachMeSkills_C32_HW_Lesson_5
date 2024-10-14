76dc931e5ed16db826df52a011bbc1879df7a0e5
Marina Yadevich <marishka.kazakova.2012@mail.ru>
2024-10-14 14:58:25 +0300

    Add Task3.

---
+++ src/Task3.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/** 3. Создайте двумерный массив целых чисел.
+ * Выведите на консоль сумму всех элементов массива.
+ */
+
+
+public class Task3 {
+    public static void main(String[] args) {
+        //                 0   1  2
+        int[][] arrays = { {3, 4, 8}, // 0
+                          {4, 78, 1}, // 1
+                          {56, 7, 90} // 2
+        };
+
+        System.out.println("Source array: " + Arrays.deepToString(arrays));
+
+        int sum = 0;
+        for(int i = 0; i < arrays.length; i++){
+            for(int j = 0; j < arrays[i].length; j++){
+                sum += arrays[i][j];
+            }
+        }
+        System.out.println("Sum of all elements of ann array: " + sum);
+    }
+}
